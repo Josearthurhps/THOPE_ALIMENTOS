f2d1b60394b38ad6e50e6735f714c6b7b5c9c6a1
Auggustto <leonardoaugusto1000a@gmail.com>
2024-05-20 15:02:48 -0300

    Foi adicionado o esquema de pastas, arquivos de envio de email e fake banco de dados

---
+++ app/fake_database.py
@@ -0,0 +1,54 @@
+
+
+
+list_product = [
+    {
+    'id': 1,
+    'name': 'Bolinho de Mandioca',
+    'description': '',
+    'price': 10.0,
+    'category': 'Bolinho',
+    'image': 'images/bolinho_de_mandioca.jpg'
+    },
+    {
+    'id': 2,
+    'name': 'CAMARÃO EMPANADO RECHEADO',
+    'description': '',
+    'price': 26.0,
+    'category': 'Bolinho',
+    'image': 'images/Camarao1.jpg'
+    },
+    {
+    'id': 3,
+    'name': 'CROQUETE DE BACALHAU RECHEADO COM REQUEIJÃO',
+    'description': '',
+    'price': 10.0,
+    'category': 'Bolinho',
+    'image': 'images/croquete.jpg'
+    },
+    {
+    'id': 4,
+    'name': 'BOLINHO DE TILÁPIA COM REQUEIJÃO',
+    'description': '',
+    'price': 10.0,
+    'category': 'Bolinho',
+    'image': 'images/bolinho_de_tilapia.jpg'
+    },
+    # {
+    # 'id': 5,
+    # 'name': 'Bolinho de Pamonha',
+    # 'description': '',
+    # 'price': 10.0,
+    # 'category': 'Bolinho',
+    # 'image': 'images/bolinho_de_mandioca.jpg'
+    # },
+    # {
+    # 'id': 6,
+    # 'name': 'Bolinho de Pamonha',
+    # 'description': '',
+    # 'price': 10.0,
+    # 'category': 'Bolinho',
+    # 'image': 'images/bolinho_de_mandioca.jpg'
+    # }
+]
+
